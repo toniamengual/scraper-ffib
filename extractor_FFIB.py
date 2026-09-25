f7f09d864c617f57d4069b69c63ba4eccffd9100
toniamengual <143012805+toniamengual@users.noreply.github.com>
2026-09-25 12:10:47 +0200

    Update extractor_FFIB.py

--- extractor_FFIB.py
+++ extractor_FFIB.py
@@ -33,8 +33,6 @@ TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
 CATEGORIAS = {
     # ── Categorías SIN calendario publicado aún ──────────────────────────────
     "Info_ESCOLETA":                  { "url": "", "filtro": "BUNYOLA",                  "telegram_channel": "-1004469746773" },
-    "Info_INFANTIL F7":               { "url": "", "filtro": "BUNYOLA",                  "telegram_channel": "-1003979746641" },
-
     # ── Categorías CON calendario temporada pasada (URLs pendientes de actualizar) ──
     "Info_BENJAMÍ 1R":                { "url": "", "filtro": "BUNYOLA",                  "telegram_channel": "-1004375714654" },  # BENJAMI BLANC
     "Info_BENJAMÍ 2ON":               { "url": "", "filtro": "BUNYOLA",                  "telegram_channel": "-1004399088046" },  # BENJAMI VERMELL
@@ -43,6 +41,7 @@ CATEGORIAS = {
     "Info_ALEVÍ BLANC PREFERENT":     { "url": "", "filtro": "BUNYOLA",                  "telegram_channel": "-1004349965204" },  # ALEVI BLANC
 
     # ── Categorías CON calendario temporada 22 ───────────────────────────────
+    "Info_INFANTIL F7":               { "url": "https://www.ffib.es/Fed/NPcd/NFG_VisCalendario_Vis?cod_primaria=1000110&codgrupo=23348437&codcompeticion=23348436&codtemporada=22&CodJornada=&CDetalle=1", "filtro": "BUNYOLA", "telegram_channel": "-1003979746641" },
     "Info_INFANTIL F11":              { "url": "https://www.ffib.es/Fed/NPcd/NFG_VisCalendario_Vis?cod_primaria=1000110&codgrupo=23651707&codcompeticion=23651704&codtemporada=22&CodJornada=&CDetalle=1", "filtro": "BUNYOLA", "telegram_channel": "-1003734265279" },  # INFANTIL F11
     "Info_PREBENJAMI":                { "url": "https://www.ffib.es/Fed/NPcd/NFG_VisCalendario_Vis?cod_primaria=1000110&codgrupo=23348692&codcompeticion=23348690&codtemporada=22&CodJornada=&CDetalle=1", "filtro": "BUNYOLA", "telegram_channel": "-1004347891667" },
     "Info_AMATEUR A":                 { "url": "https://www.ffib.es/Fed/NPcd/NFG_VisCalendario_Vis?cod_primaria=1000110&codgrupo=23348366&codcompeticion=23348365&codtemporada=22&CodJornada=&CDetalle=1", "filtro": "BUNYOLA", "telegram_channel": "-1003877588580" },
